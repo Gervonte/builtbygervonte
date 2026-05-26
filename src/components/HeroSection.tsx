6a4218fcc97e6b3fbf0cd987fe27fecfa2ae14ce
Gervonte Fowler <hello@builtbygervonte.com>
2026-05-26 16:22:00 -0400

    chore: update aria label

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -60,7 +60,8 @@ const HeroSection = memo(() => {
                   color: commonColors.textSecondary,
                 }}
                 role="text"
-                aria-label="Software, content, and systems built by a creative engineer born and raised in The Bahamas"
+                aria-label="Full-stack software engineer building fintech products, technical content, and 0 to
+                1 creative systems."
               >
                 Full-stack software engineer building fintech products, technical content, and 0 to
                 1 creative systems.
